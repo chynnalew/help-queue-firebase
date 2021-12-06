56d542f0ec9ee27f269e1c480a71c6431c12bad8
chynnalew <chynnalew@yahoo.com>
2021-12-06 10:06:54 -0800

    refactor ticketControl to show inividual firestore tickets

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -6,6 +6,7 @@ import EditTicketForm from './EditTicketForm';
 import { connect } from 'react-redux';
 import PropTypes from "prop-types";
 import * as a from './../actions';
+import { withFirestore } from 'react-redux-firebase';
 
 class TicketControl extends React.Component {
 
@@ -59,8 +60,15 @@ class TicketControl extends React.Component {
   }
 
   handleChangingSelectedTicket = (id) => {
-    const selectedTicket = this.props.mainTicketList[id];
-    this.setState({selectedTicket: selectedTicket});
+    this.props.firestore.get({ collection: 'tickets', doc: id }).then((ticket) => {
+      const firestoreTicket = {
+        names: ticket.get('names'),
+        location: ticket.get('location'),
+        issue: ticket.get('issue'),
+        id: ticket.id
+      }
+      this.setState({ selectedTicket: firestoreTicket });
+    });
   }
 
   handleDeletingTicket = (id) => {
@@ -110,17 +118,17 @@ class TicketControl extends React.Component {
 }
 
 TicketControl.propTypes = {
-  mainTicketList: PropTypes.object,
+  //mainTicketList: PropTypes.object,
   formVisibleOnPage: PropTypes.bool
 };
 
 const mapStateToProps = state => {
   return {
-    mainTicketList: state.mainTicketList,
+    //mainTicketList: state.mainTicketList,
     formVisibleOnPage: state.formVisibleOnPage
   }
 }
 
 TicketControl = connect(mapStateToProps)(TicketControl);
 
-export default TicketControl;
+export default withFirestore(TicketControl);
